4bf2ff1d1c75ca5c1d6999f4fddbe81f48e7ec29
Eligijus Blankus <eligijusblankus@Eligijuss-MacBook-Pro.local>
2017-11-29 11:56:30 +0000

    deleted login,logout and base html, setup urls

--- theapp/urls.py
+++ theapp/urls.py
@@ -1,4 +1,4 @@
-"""web URL Configuration
+"""lab8 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^newspapers/', include('theapp.urls')),
+    url(r'^$', views.index, name='index'),
 ]
